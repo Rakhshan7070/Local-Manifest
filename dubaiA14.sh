00aeafce84f7aff8f4802db06bd16c7a37d9bed8
Rakhshan Ali <121305132+Rakhshan7070@users.noreply.github.com>
2024-11-11 01:45:16 +0530

    dubai: Specify A14 branches

--- dubaiA14.sh
+++ dubaiA14.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/Rakhshan7070/android_device_motorola_dubai.git device/motorola/dubai
+git clone https://github.com/Rakhshan7070/android_device_motorola_dubai.git -b lineage-21-U14 device/motorola/dubai
 
-git clone https://github.com/Rakhshan7070/android_device_motorola_sm7325-common device/motorola/sm7325-common
+git clone https://github.com/Rakhshan7070/android_device_motorola_sm7325-common.git -b lineage-21-U14 device/motorola/sm7325-common
 
-git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_dubai vendor/motorola/dubai
+git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_dubai.git -b lineage-21-U14 vendor/motorola/dubai
 
-git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_sm7325-common vendor/motorola/sm7325-common
+git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_sm7325-common.git -b lineage-21-U14 vendor/motorola/sm7325-common
 
-git clone https://github.com/Rakhshan7070/android_external_bson external/bson
+git clone https://github.com/Rakhshan7070/android_external_bson.git -b lineage-21 external/bson
 
-git clone https://github.com/Rakhshan7070/android_system_qcom system/qcom
+git clone https://github.com/Rakhshan7070/android_system_qcom.git -b lineage-21 system/qcom
 
-git clone https://github.com/Rakhshan7070/android_hardware_motorola hardware/motorola
+git clone https://github.com/Rakhshan7070/android_hardware_motorola.git -b lineage-21 hardware/motorola
