5cef7399e915264fe14751f0af68bf9537422f53
Bashid <alirakhshan7070@gmail.com>
2024-12-15 16:23:42 +0530

    miami: Update repo name & branches
    - remove wip tag: build & tested.
    - changed repo name.
    - Added audio qcom-caf-8350 to fix -wno errors.

--- miamiA15.sh
+++ miamiA15.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-git clone https://github.com/Rakhshan7070/device_motorola_miami.git -b lineage-22-wip device/motorola/miami
+git clone https://github.com/Rakhshan7070/android_device_motorola_miami.git -b lineage-22 device/motorola/miami
 
-git clone https://github.com/Rakhshan7070/device_motorola_sm6375-common.git -b lineage-22-wip device/motorola/sm6375-common
+git clone https://github.com/Rakhshan7070/android_device_motorola_sm6375-common.git -b lineage-22 device/motorola/sm6375-common
 
-git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_miami.git -b lineage-22-wip vendor/motorola/miami
+git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_miami.git -b lineage-22 vendor/motorola/miami
 
-git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_sm6375-common.git -b lineage-22-wip vendor/motorola/sm6375-common
+git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_sm6375-common.git -b lineage-22 vendor/motorola/sm6375-common
 
-git clone https://github.com/Rakhshan7070/kernel_motorola_miami.git -b lineage-22-wip kernel/motorola/miami
+git clone https://github.com/Rakhshan7070/android_kernel_motorola_sm6375.git -b lineage-22 kernel/motorola/miami
 
 git clone https://github.com/Rakhshan7070/android_external_bson.git -b lineage-22 external/bson
 
 git clone https://github.com/Rakhshan7070/android_system_qcom.git -b lineage-22 system/qcom
 
 git clone https://github.com/Rakhshan7070/android_hardware_motorola.git -b lineage-22 hardware/motorola
+
+rm -rf hardware/qcom-caf/sm8350/audio
+
+git clone https://github.com/Rakhshan7070/android_hardware_qcom_audio.git -b lineage-22-caf-sm8350 hardware/qcom-caf/sm8350/audio
+
