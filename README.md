a708b3d8ce3f50561142df2a44e536b50ca77d9c
Rakhshan Ali <alirakhshan7070@gmail.com>
2024-12-21 18:47:56 +0530

    dubai: update branches to the new naming scheme

--- README.md
+++ README.md
@@ -4,16 +4,16 @@
 Assuming already synced Sources (A14):
  - cd into your ROM's folder
 ```
-    curl https://raw.githubusercontent.com/Rakhshan7070/Local-Manifest/lineage-21/dubaiA14.sh | bash
+    curl https://raw.githubusercontent.com/Rakhshan7070/Local-Manifest/14.0/dubaiA14.sh | bash
 ```
 --- To Build with KernelSU :
 ```
-    git clone https://github.com/Rakhshan7070/android_kernel_motorola_sm7325.git -b lineage-21-T13-KSU kernel/motorola/sm7325/
+    git clone https://github.com/Rakhshan7070/android_kernel_motorola_sm7325.git -b 14.0-KSU kernel/motorola/sm7325/
     
     git clone https://github.com/tiann/KernelSU.git -b v0.9.4 kernel/motorola/sm7325/KernelSU/
 ```
 --- To Build without KernelSU :
 ```
-    git clone https://github.com/Rakhshan7070/android_kernel_motorola_sm7325.git -b lineage-21 kernel/motorola/sm7325/
+    git clone https://github.com/Rakhshan7070/android_kernel_motorola_sm7325.git -b 14.0 kernel/motorola/sm7325/
 ```
 
--- dubaiA14.sh
+++ dubaiA14.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/Rakhshan7070/android_device_motorola_dubai.git -b lineage-21-U14 device/motorola/dubai
+git clone https://github.com/Rakhshan7070/android_device_motorola_dubai.git -b 14.0 device/motorola/dubai
 
-git clone https://github.com/Rakhshan7070/android_device_motorola_sm7325-common.git -b lineage-21-U14 device/motorola/sm7325-common
+git clone https://github.com/Rakhshan7070/android_device_motorola_sm7325-common.git -b 14.0 device/motorola/sm7325-common
 
-git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_dubai.git -b lineage-21-U14 vendor/motorola/dubai
+git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_dubai.git -b 14.0 vendor/motorola/dubai
 
-git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_sm7325-common.git -b lineage-21-U14 vendor/motorola/sm7325-common
+git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_sm7325-common.git -b 14.0 vendor/motorola/sm7325-common
 
-git clone https://github.com/Rakhshan7070/android_hardware_motorola.git -b lineage-21 hardware/motorola
+git clone https://github.com/Rakhshan7070/android_hardware_motorola.git -b 14.0 hardware/motorola
