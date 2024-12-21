1732953d32bfa5a500282e58cb2802c4e2c65106
Rakhshan Ali <alirakhshan7070@gmail.com>
2024-12-21 18:44:16 +0530

    Moto Common: don't clone external_bson and system_qcom
    These repos are merged in android_hardware_motorola, therefore not needed to be cloned.

--- dubaiA14.sh
+++ dubaiA14.sh
@@ -8,8 +8,4 @@ git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_dubai.git 
 
 git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_sm7325-common.git -b lineage-21-U14 vendor/motorola/sm7325-common
 
-git clone https://github.com/Rakhshan7070/android_external_bson.git -b lineage-21 external/bson
-
-git clone https://github.com/Rakhshan7070/android_system_qcom.git -b lineage-21 system/qcom
-
 git clone https://github.com/Rakhshan7070/android_hardware_motorola.git -b lineage-21 hardware/motorola
