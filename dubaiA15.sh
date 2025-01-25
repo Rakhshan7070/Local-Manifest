4af6b00f489bce7eda01dc20752289f3cd0ac2c5
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-01-25 23:34:08 +0530

    dubai: clone vendor from gitlab remote

--- dubaiA15.sh
+++ dubaiA15.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Rakhshan7070/android_device_motorola_dubai.git -b 1
 
 git clone https://github.com/Rakhshan7070/android_device_motorola_sm7325-common.git -b 15.0 device/motorola/sm7325-common
 
-git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_dubai.git -b 15.0 vendor/motorola/dubai
+git clone https://gitlab.com/Rakhshan7070/proprietary_vendor_motorola_dubai.git -b 15.0 vendor/motorola/dubai
 
 git clone https://github.com/Rakhshan7070/proprietary_vendor_motorola_sm7325-common.git -b 15.0 vendor/motorola/sm7325-common
 
